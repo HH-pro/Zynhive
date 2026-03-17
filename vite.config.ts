d4dc8b4fea3e95709675adf54373cbcb29f69187
HH-pro <hamzamanzoor0011@gmail.com>
2026-03-18 01:54:36 +0500

    Auto update

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,15 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+
+  // ✅ IMPORTANT: assets path fix karega (Vercel ke liye)
+  base: "/",
+
   build: {
     rollupOptions: {
       output: {
         manualChunks: {
-          react:  ["react", "react-dom"],
+          react: ["react", "react-dom"],
         },
       },
     },
