4cf7521261385dfa8c4993573af6d4d372c87fe5
HH-pro <hamzamanzoor0011@gmail.com>
2026-03-27 04:33:59 +0500

    Auto update

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -94,7 +94,7 @@ export function Navbar({ dark, onToggle, currentPage = "/" }: NavbarProps) {
             style={{
               height: scrolled ? 32 : 40,
               width: "auto",
-              maxWidth: 460,
+              maxWidth: 60,
               transition: "height .35s var(--ease), opacity .2s ease",
             }}
           />
